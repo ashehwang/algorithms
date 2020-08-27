d7399f1ae931f061a0ef6102ffa8a6aaec931653
Ashe <ashe@nyu.edu>
2020-08-27 15:23:26 -0700

    algorithm

--- other_algorithms/ashe.js
+++ other_algorithms/ashe.js
@@ -139,3 +139,100 @@ function isNewRegion(node, graph, visited) {
 module.exports = {
     numRegions
 };
+
+
+class Link {
+    constructor(value) {
+        this.next = null;
+        this.prev = null;
+        this.value = value;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = new Link();
+        this.tail = new Link();
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+    isEmpty() {
+        return this.head.next === this.tail;
+    }
+
+    first() {
+        if (this.isEmpty()) return null;
+        return this.head.next;
+    }
+
+    last() {
+        if (this.isEmpty()) return null;
+        return this.tail.prev;
+    }
+
+    find(val) {
+        let link = this.first();
+        while (link) {
+            if (link.value === val) {
+                return link;
+            }
+
+            link = link.next;
+        }
+    }
+
+    remove(val) {
+        let nodeToRemove = this.find(val);
+        if (!nodeToRemove) return;
+        let before = nodeToRemove.prev;
+        let after = nodeToRemove.next;
+
+        before.next = after;
+        after.prev = before;
+        nodeToRemove.prev = null;
+        nodeToRemove.next = null;
+
+        return nodeToRemove;
+    }
+
+    push(val) {
+        const link = new Link(val);
+        let last = this.tail.prev;
+        last.next = link;
+        link.prev = last;
+        link.next = this.tail;
+        this.tail.prev = link;
+    }
+
+    pop() {
+        if (this.isEmpty()) return;
+        const last = this.last();
+        const newLast = last.prev;
+
+        newLast.next = this.tail;
+        this.tail.prev = newLast;
+        last.next = null;
+        last.prev = null;
+        return last;
+    }
+
+    removeAll(val) {
+        let link = this.head.next;
+
+        while (link !== this.tail) {
+            if (link.value === val) {
+                let before = link.prev;
+                let after = link.next;
+
+                before.next = after;
+                after.prev = before;
+                link.prev = null;
+                link.next = null;
+                link = after;
+            } else {
+                link = link.next;
+            }
+        }
+    }
+}
